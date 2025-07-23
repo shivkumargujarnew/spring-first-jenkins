2a0df16fd413ae3a3ca654c724d4ecbf14d0efa3
shivkumar <shivkumargujar16@gmail.com>
2025-07-23 17:36:05 +0530

    first time commit

---
+++ src/main/java/com/example/demo/JenkinProjectFirstApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import java.util.logging.Logger;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.logging.slf4j.SLF4JLogger;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinProjectFirstApplication {
+	 private static  Log logger = LogFactory.getLog(JenkinProjectFirstApplication.class);
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinProjectFirstApplication.class, args);
+	   System.out.println("hello welocomw to first jenkin project");
+	    logger.info("hello welocomw to first jenkin project" );
+	}
+
+}
